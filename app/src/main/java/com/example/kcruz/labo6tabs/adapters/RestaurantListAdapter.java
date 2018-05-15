07345106d70cd69eaccb23a4ce9165baf1008d18
00010216 <00010216@uca.edu.sv>
2018-05-15 00:36:08 -0600

    agregando funcionalidad de favoritos

--- app/src/main/java/com/example/kcruz/labo6tabs/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/example/kcruz/labo6tabs/adapters/RestaurantListAdapter.java
@@ -11,49 +11,105 @@ import android.widget.TextView;
 
 import com.example.kcruz.labo6tabs.R;
 import com.example.kcruz.labo6tabs.models.Restaurant;
+import com.example.kcruz.labo6tabs.utils.SharedPreference;
 
 import java.util.List;
 
 public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder>{
-    private Context context;
-    List<Restaurant> restaurants;
+    private Context mContext;
+    List<Restaurant> mRestaurants;
+    SharedPreference sharedPreference;
 
-    public RestaurantListAdapter(Context mContext, List<Restaurant> mRestaurants) {
-        context = mContext;
-        restaurants = mRestaurants;
+    public interface RestaurantListClickListener{
+        public void onProductClick(View v, int position);
+        public void onProductLongClick(View v, int position);
+
     }
+    private RestaurantListClickListener mListener;
+
+    public RestaurantListAdapter(Context context, List<Restaurant> restaurants, RestaurantListClickListener mListener){
+        mContext = context;
+        mRestaurants = restaurants;
+        this.mListener = mListener;
+        sharedPreference = new SharedPreference();
+    }
+
 
     public static class RestaurantViewHolder extends RecyclerView.ViewHolder {
         //indica la vista donde se ubicaran la informaciond de la lista
         CardView card;
         ImageView img;
-        TextView title,description;
+        TextView title;
+        ImageView favoriteImg;
 
         public RestaurantViewHolder(View itemView) {
             super(itemView);
             card = itemView.findViewById(R.id.card_view);
             img = itemView.findViewById(R.id.img);
             title = itemView.findViewById(R.id.title);
-            description = itemView.findViewById(R.id.description);
+            favoriteImg = itemView.findViewById(R.id.imgbtn_favorite);
+
         }
     }
 
     @Override
     public RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.cardview_item,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item,parent,false);
         return (new RestaurantViewHolder(view));
     }
 
     @Override
-    public void onBindViewHolder(RestaurantViewHolder holder, int position) {
-        holder.img.setImageResource(restaurants.get(position).getPhoto());
-        holder.title.setText(restaurants.get(position).getName());
-        holder.description.setText(restaurants.get(position).getDescription());
+    public void onBindViewHolder(RestaurantViewHolder holder, final int position) {
+        final Restaurant restaurant = (Restaurant) mRestaurants.get(position);
+        holder.img.setImageResource(mRestaurants.get(position).getPhoto());
+        holder.title.setText(mRestaurants.get(position).getName());
+
+        if (checkFavoriteItem(restaurant)) {
+            holder.favoriteImg.setImageResource(R.drawable.ic_fav);
+            holder.favoriteImg.setTag("red");
+        } else {
+            holder.favoriteImg.setImageResource(R.drawable.ic_fav_border);
+            holder.favoriteImg.setTag("grey");
+        }
+
+        holder.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onProductClick(v, position);
+            }
+        });
+
+        holder.favoriteImg.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mListener.onProductLongClick(v, position);
+                return true;
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return restaurants.size();
+        return mRestaurants.size();
     }
 
+    public boolean checkFavoriteItem(Restaurant checkRestaurant) {
+        boolean check = false;
+        List<Restaurant> favorites = sharedPreference.getFavorites(mContext);
+        if (favorites != null) {
+            for (Restaurant restaurant : favorites) {
+                if (restaurant.equals(checkRestaurant)) {
+                    check = true;
+                    break;
+                }
+            }
+        }
+        return check;
+    }
+
+    public void remove(Restaurant restaurant) {
+        //super.remove(product);
+        mRestaurants.remove(restaurant);
+        notifyDataSetChanged();
+    }
 }
